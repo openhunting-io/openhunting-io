ed160376173545a759e9f61eb63bffedae50701f
Rizqy Rionaldy <rionaldyrizqy@gmail.com>
2023-09-28 09:18:28 +0700

    upload js

---
+++ inidata1.js
@@ -0,0 +1,50 @@
+var inidata1 = {
+    "name": "ZXShell",
+    "alias": "ZXShell, Sensocode",
+    "category": "Malware",
+    "type": "Reconnaissance, Backdoor, Keylogger, Info stealer, Exfiltration, Tunneling, DDoS",
+    "modified": "2020-05-14",
+    "all_data": {
+        "tool": "ZXShell",
+        "names": [
+            {
+                "name": "ZXShell"
+            },
+            {
+                "name": "Sensocode"
+            }
+        ],
+        "description": "(FireEye) ZXSHELL is a backdoor that can be downloaded from the internet, particularly Chinese hacker websites. The backdoor can launch port scans, run a keylogger, capture screenshots, set up an HTTP or SOCKS proxy, launch a reverse command shell, cause SYN floods, and transfer/delete/run files. The publicly available version of the tool provides a graphical user interface that malicious actors can use to interact with victim backdoors. Simplified Chinese is the language used for the bundled ZXSHELL documentation.",
+        "category": "Malware",
+        "type": [
+            "Reconnaissance",
+            "Backdoor",
+            "Keylogger",
+            "Info stealer",
+            "Exfiltration",
+            "Tunneling",
+            "DDoS"
+        ],
+        "information": [
+            "https://paper.bobylive.com/Security/APT_Report/APT-41.pdf",
+            "https://github.com/smb01/zxshell",
+            "https://blogs.cisco.com/security/talos/opening-zxshell"
+        ],
+        "mitre-attack": [
+            "https://attack.mitre.org/software/S0412/"
+        ],
+        "malpedia": [
+            "https://malpedia.caad.fkie.fraunhofer.de/details/win.zxshell"
+        ],
+        "alienvault-otx": [
+            "https://otx.alienvault.com/browse/pulses?q=tag:zxshell"
+        ],
+        "uuid": "b63bf358-4d19-4729-b6bb-dfd6588f44e0",
+        "last-card-change": "2020-05-14",
+        "source": "https://apt.etda.or.th/cgi-bin/listtools.cgi"
+    },
+    "ioc_data": [],
+    "last_ioc_update": null,
+    "file_name": "zxshell",
+    "analysis": null
+}
