e411d9e27572f5d9cb4e29f3f22efb087f73e8b2
Rizqy Rionaldy <rionaldyrizqy@gmail.com>
2023-11-02 00:52:56 +0700

    update template

---
+++ data/threat-tools.js
@@ -0,0 +1,9 @@
+const threatTools = [
+    {
+      title: "Cyber Threat Intelligence on Budget: Checking Trojan Account Compromise",
+      imageSrc: "data/image-threat-tools/cti_onbudget_compromise account.png",
+      downloadLink: "https://example.com/download1",
+      manualLink: "https://example.com/manual1",
+    },
+];
+  
