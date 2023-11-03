56d20bf371b5ad5b834d777733e4ebdb0894521d
Rizqy Rionaldy <rionaldyrizqy@gmail.com>
2023-11-03 11:14:24 +0700

    update template

---
+++ data/threat-report.js
@@ -0,0 +1,18 @@
+var threatReport = [
+    {
+        date: '23 January 2023',
+        title: 'Analyst Reporting Cyber Security Awareness',
+        category: 'Malware',
+        analyst: 'Rizqy Rionaldy',
+        action: '<a href="">Download</a>'
+    },
+    {
+        date: '24 January 2023',
+        title: 'Test',
+        category: 'Malware',
+        analyst: 'Rizqy Rionaldy',
+        action: '<a href="">Download</a>'
+    },
+    
+  ];
+  
