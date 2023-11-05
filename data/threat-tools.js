fc3d00e51eea0313bea63725b958e6c3364ca05f
Rizqy Rionaldy <rionaldyrizqy@gmail.com>
2023-11-05 18:21:13 +0700

    update

--- data/threat-tools.js
+++ data/threat-tools.js
@@ -1,6 +1,6 @@
 const threatTools = [
     {
-      title: "Threathunt Exposure Data Breach Monitoring",
+      title: "OHCTI! Exposure Data Breach Monitoring",
       imageSrc: "data/image-threat-tools/tools1.png",
       downloadLink: "https://example.com/download1",
       manualLink: "https://example.com/manual1",
