69260f2b5a187cafc1a8993a45217e0e3d3eb8fa
Rizqy Rionaldy <rionaldyrizqy@gmail.com>
2023-11-23 00:07:45 +0700

    update threat report

--- data/threat-report.js
+++ data/threat-report.js
@@ -6,6 +6,13 @@ var threatReport = [
         analyst: 'Rizqy Rionaldy',
         action: '<a href="data/file-threat-report/A COMPREHENSIVE EXAMINATION OF COMMON VULNERABILITY & EXPOSURE TRENDS IN ISRAEL PALESTINE.pdf">Download</a>'
     },
+    {
+        date: '23 November 2023',
+        title: 'GAMBLING CAMPAIGN WEB DEFACEMENT, LESSON LEARNING FROM WEB DEFACEMENT IN INDONESIA',
+        category: 'Web Defacement',
+        analyst: 'Rizqy Rionaldy',
+        action: '<a href="data/file-threat-report/Gambling Campaign Web Defacement - Lesson Learning From Web Defacement in Indonesia.pdf">Download</a>'
+    },
     
   ];
   
