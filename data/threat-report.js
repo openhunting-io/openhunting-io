26379dfd174d8ee57af624895a87919905f1949d
Rizqy Rionaldy <rionaldyrizqy@gmail.com>
2023-12-03 21:23:18 +0700

    add new article

--- data/threat-report.js
+++ data/threat-report.js
@@ -1,5 +1,12 @@
 var threatReport = [
     {
+        date: '3 December 2023',
+        title: 'Threat Hunting Malware Infrastructure',
+        category: 'Threat Hunting',
+        analyst: 'Rizqy Rionaldy',
+        action: '<a href="data/file-threat-report/Threat Hunting Ransomware Infrastructure.pdf">Download</a>'
+    },
+    {
         date: '23 November 2023',
         title: 'GAMBLING CAMPAIGN WEB DEFACEMENT, LESSON LEARNING FROM WEB DEFACEMENT IN INDONESIA',
         category: 'Web Defacement',
