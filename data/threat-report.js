f6ccfcd6ebfd4a2f189996a7a1d806c3ea45d0f0
Rizqy Rionaldy <rionaldyrizqy@gmail.com>
2023-12-03 19:37:15 +0700

    add Malware Infrastructure Threat Hunting Tools

--- data/threat-report.js
+++ data/threat-report.js
@@ -1,18 +1,17 @@
 var threatReport = [
     {
-        date: '16 November 2023',
-        title: 'A COMPREHENSIVE EXAMINATION OF COMMON VULNERABILITY & EXPOSURE TRENDS IN ISRAEL PALESTINE',
-        category: 'Common Vulnerability Exposure',
-        analyst: 'Rizqy Rionaldy',
-        action: '<a href="data/file-threat-report/A COMPREHENSIVE EXAMINATION OF COMMON VULNERABILITY & EXPOSURE TRENDS IN ISRAEL PALESTINE.pdf">Download</a>'
-    },
-    {
         date: '23 November 2023',
         title: 'GAMBLING CAMPAIGN WEB DEFACEMENT, LESSON LEARNING FROM WEB DEFACEMENT IN INDONESIA',
         category: 'Web Defacement',
         analyst: 'Rizqy Rionaldy',
         action: '<a href="data/file-threat-report/Gambling Campaign Web Defacement - Lesson Learning From Web Defacement in Indonesia.pdf">Download</a>'
     },
-    
+    {
+        date: '16 November 2023',
+        title: 'A COMPREHENSIVE EXAMINATION OF COMMON VULNERABILITY & EXPOSURE TRENDS IN ISRAEL PALESTINE',
+        category: 'Common Vulnerability Exposure',
+        analyst: 'Rizqy Rionaldy',
+        action: '<a href="data/file-threat-report/A COMPREHENSIVE EXAMINATION OF COMMON VULNERABILITY & EXPOSURE TRENDS IN ISRAEL PALESTINE.pdf">Download</a>'
+    }    
   ];
   
--- data/threat-tools.js
+++ data/threat-tools.js
@@ -1,9 +1,15 @@
 const threatTools = [
     {
+      title: "Malware Infrastructure Threat Hunting",
+      imageSrc: "data/image-threat-tools/tools2.png",
+      downloadLink: "https://github.com/openhunting-io/ohcti-malwareinfra",
+      manualLink: "https://github.com/openhunting-io/ohcti-malwareinfra",
+    },
+    {
       title: "OHCTI! Exposure Data Breach Monitoring",
       imageSrc: "data/image-threat-tools/tools1.png",
       downloadLink: "https://github.com/openhunting-io/ohcti-threatexposure",
       manualLink: "https://github.com/openhunting-io/ohcti-threatexposure",
-    },
+    }
 ];
   
